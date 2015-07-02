ced7804ff97d969391ef38ea89fc07a58bdafc9d
Marvin Smith <marvin.smith@sncorp.com>
2015-07-02 11:43:09 -0700

    Cleaning up the Render state and driver objects.  Also fixed a bug in the Render Driver where a quick return on a method would not refresh the status bar properly.

--- src/lib/cli_cpp/handlers/A_CLI_Resize_Command_Response_Handler.cpp
+++ src/lib/cli_cpp/handlers/A_CLI_Resize_Command_Response_Handler.cpp
@@ -42,6 +42,9 @@ bool A_CLI_Resize_Command_Response_Handler::Is_Supported( CLI::CMD::A_Command_Re
 /****************************************/
 void A_CLI_Resize_Command_Response_Handler::Process_Command( CLI::CMD::A_Command_Result::ptr_t response ){
 
+
+    std::cout << "Inside A_CLI_Resize_Command_Response_Handler" << std::endl;
+
     // Define our values
     int rows         = response->Get_Argument_Value<int>( 0 );
     int cols         = response->Get_Argument_Value<int>( 1 );
